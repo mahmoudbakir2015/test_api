6ae40a444483cda65b23b9561e380754fb2c53d2
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-04-01 14:31:04 +0200

    add observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:api_test/cubit/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:api_test/cubit/user_cubit.dart';
 import 'package:api_test/screens/sign_in_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   runApp(
     BlocProvider(
       create: (context) => UserCubit(),
